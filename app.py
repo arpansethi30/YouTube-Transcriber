8e122ed3c01033f3cb0174a649e69d62c52a78ce
Arpan Sethi <arpansethi30@gmail.com>
2024-06-28 23:52:24 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,16 +1,61 @@
 import streamlit as st
 from dotenv import load_dotenv
-
-load_dotenv()
 import os
 import google.generativeai as genai
+from youtube_transcript_api import YouTubeTranscriptApi
+
+load_dotenv()  ## Load all the environment variables
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-prompt = """You are a YouTube video summarizer. You will be taking transcript text and summarizing it into a short, concise summary."""
+prompt = """You are YouTube video summarizer. You will be taking the transcript text
+and summarizing the entire video and providing the important summary in points
+within 250 words. Please provide the summary of the text given here:  """
 
 
+## Getting the transcript data from YouTube videos
+def extract_transcript_details(youtube_video_url):
+    try:
+        video_id = youtube_video_url.split("=")[1]
+        transcript_text = YouTubeTranscriptApi.get_transcript(video_id)
+
+        transcript = ""
+        for i in transcript_text:
+            transcript += " " + i["text"]
+
+        return transcript
+
+    except Exception as e:
+        raise e
+
+
+## Getting the summary based on Prompt from Google Gemini Pro
 def generate_gemini_content(transcript_text, prompt):
-    model = genai.load_model("gemini-pro")
-    response = model.generate(prompt + transcript_text)
-    return response.text
+    try:
+        model = genai.GenerativeModel("gemini-pro")
+        response = model.generate_content(prompt + transcript_text)
+        # Print the response to inspect its structure
+        print(response)
+        # Assuming the response structure has a 'content' field or similar
+        return response.get(
+            "content", "Error: Unable to retrieve content from response"
+        )
+    except Exception as e:
+        print(f"Error generating content: {e}")
+        return "Error: An issue occurred while generating the content."
+
+
+st.title("YouTube Transcript to Detailed Notes Converter")
+youtube_link = st.text_input("Enter YouTube Video Link:")
+
+if youtube_link:
+    video_id = youtube_link.split("=")[1]
+    st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
+
+if st.button("Get Detailed Notes"):
+    transcript_text = extract_transcript_details(youtube_link)
+
+    if transcript_text:
+        summary = generate_gemini_content(transcript_text, prompt)
+        st.markdown("## Detailed Notes:")
+        st.write(summary)
